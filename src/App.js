594d57664e7db5adc94f1c165f21905bdc7f9d69
Pedro Abreu Arenas <pfabreuave@gmail.com>
2022-04-11 16:38:56 -0300

    Reinstalacion pagina de presentacion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='main-container'>
+			<div className='container'>
+				<div className='prolife-pic'></div>
+				<div className='layer'></div>
+				<div className='detail'>
+					<p>Pedro Abreu Arenas</p>
+					<p>Especialista em TI</p>
+				</div>
+				<div>
+					<p>Telf: 55 21 969615392</p>
+					<p>pfabreuave@gmail.com</p>
+				</div>			
+				
+
+				<table>
+    				<tr>
+        				<td>
+							<p className='fa-mainframe'></p>
+        				</td>
+        				<td>
+							<p className='fa-jcl'></p>
+        				</td>
+						<td>
+							<p className='fa-vsam'></p>
+        				</td>
+        				<td>
+							<p className='fa-cics'></p>
+        				</td>
+						<td>
+							<p className='fa-cobol'></p>
+        				</td>
+						<td>
+							<p className='fa-adanat'></p>
+        				</td>
+    				</tr>
+				</table>
+
+				<div className='skills'>
+					<i className='fab fa-html5'></i>
+					<i className='fab fa-css3-alt'></i>
+					<i className='fab fa-js'></i>
+					<i className='fab fa-react'></i>
+					<i className='fab fa-angular'></i>
+					<i className='fas fa-database'></i>
+					<i className='fab fa-node'></i>
+				</div>
+				
+				<div className='btn'>
+					<a href='https://port-mundo-rt.web.app/' target='blank_'>Disponível</a>
+				</div>
+			</div>
+	</div>
   );
 }
 
